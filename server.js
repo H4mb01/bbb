7647f040ea1393be763c7d7e78192e8e93dca639
H4mb01 <manuel@muenstedt.de>
2021-12-21 14:12:35 +0100

    small fix in authenticateToken

--- server.js
+++ server.js
@@ -134,6 +134,8 @@ app.get('/posts', authenticateToken, (req, res) => {
 function authenticateToken(req, res, next) {
     const authHeader = req.headers['authorization']
     const token = authHeader && authHeader.split(' ')[1]
+    console.log("headers:", req.headers)
+    console.log("authHeader:", authHeader, "token:", token)
     if (token == null) return res.sendStatus(401)
 
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
